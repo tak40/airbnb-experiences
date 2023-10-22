a71c8ff7d8ad78515f73092fe25160e684df73bf
Tak <takeshiseodev@gmail.com>
2023-10-22 19:40:10 -0400

    starting from scratch

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
 import data from "./data"
 
-export default function App() {
-    const cards = data.map(item => {
-        return (
-            <Card
-                key={item.id}
-                {...item}
-                
-            />
-        )
-    })        
-    
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <section className="cards-list">
-                {cards}
-            </section>
-        </div>
-    )
+function App() {
+    return <h1>something</h1>
 }
+
+export default App
